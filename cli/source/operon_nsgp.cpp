57775816304b5df7a2f64e1505693a1fdf17a2fe
Robot <ivor_denhamdyson@protonmail.com>
2022-03-23 01:32:19 +0000

    Fix

--- cli/source/operon_nsgp.cpp
+++ cli/source/operon_nsgp.cpp
@@ -337,7 +337,7 @@ auto main(int argc, char** argv) -> int
             auto elapsed = static_cast<double>(std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count()) / 1e6;
 
             fmt::print("{:.4f}\t{}\t", elapsed, gp.Generation());
-            fmt::print("{:.4f}\t{:.4f}\t{:.4g}\t{:.4g}\t{:.4g}\t{:.4g}\t{:.4g}\t{:.4g}\t", r2Train, r2Test, maeTrain, maeTest, nmseTrain, nmseTest, marginTrain, marginTest);
+            fmt::print("{:.4f}\t{:.4f}\t{:.4g}\t{:.4g}\t{:.4g}\t{:.4g}\t{:.4g}\t{:.4g}\t", r2Train, r2Test, maeTrain, maeTest, nmseTrain, nmseTest, errorTrain, errorTest);
             fmt::print("{:.4g}\t{:.1f}\t{:.3f}\t{:.3f}\t{}\t{}\t{}\t", avgQuality, avgLength, /* shape */ 0.0, /* diversity */ 0.0, evaluator.FitnessEvaluations(), evaluator.LocalEvaluations(), evaluator.TotalEvaluations());
             fmt::print("{}\t{}\n", totalMemory, config.Seed);
         };
